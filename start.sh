1bcbd5f6c8fad26f8d3ae22b9103a0fdf04a3b12
Vinh Ngu <vinh-ngu@hotmail.com>
2019-05-08 18:55:43 +0200

    Add prefix 'PLIUGIN_' to setting envs.

--- start.sh
+++ start.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -e
 
-if [[ -z $KONTENA_URL ]]; then
+if [[ -z $PLUGIN_KONTENA_URL ]]; then
     echo "Please set KONTENA_URL."
     exit 1
-elif [[ -z $KONTENA_TOKEN ]]; then
+
+elif [[ -z $PLUGIN_KONTENA_TOKEN ]]; then
     echo "Please set KONTENA_TOKEN."
     exit 1
     # OPTIONAL option
+
 elif [[ -z $PLUGIN_KONTENA_FILE ]]; then
     echo "Please set KONTENA_FILE. Otherwise kontena.yml (default) will be set."
     export PLUGIN_KONTENA_FILE='kontena.yml'
-elif [[ -z $KONTENA_GRID ]]; then
+
+elif [[ -z $PLUGIN_KONTENA_GRID ]]; then
     echo "Please set KONTENA_GRID."
     exit 1
+
 elif [[ -z $PLUGIN_KONTENA_STACK ]]; then
     echo "Please set KONTENA_STACK."
     exit 1
